5fe03d636f8e445cb38dc024894930a25cbb809a
Leo Riesenbach <leoriesenbach@gmail.com>
2020-03-15 18:29:59 -0400

    recording is now highest priority, hotel said it should help

--- include/tasks.hpp
+++ include/tasks.hpp
@@ -9,4 +9,4 @@
 static pros::Task contScreen (contScreenTask, (void *)"", TASK_PRIORITY_DEFAULT - 2, TASK_STACK_DEPTH_DEFAULT, "contScreen");
 static pros::Task autonButton (guiManagerTask, (void *)"", TASK_PRIORITY_DEFAULT - 2, TASK_STACK_DEPTH_DEFAULT, "autonButton");
 
-static pros::Task rerunRecord (recordRerun, (void *)"", TASK_PRIORITY_DEFAULT - 2, TASK_STACK_DEPTH_DEFAULT, "rerunRecord");
+static pros::Task rerunRecord (recordRerun, (void *)"", TASK_PRIORITY_DEFAULT + 1, TASK_STACK_DEPTH_DEFAULT, "rerunRecord");
